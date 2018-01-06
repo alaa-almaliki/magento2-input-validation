93b2ce72c160ea8453c88650399fd6fb462f607c
alaa-almaliki <alaa.almaliki@gmail.com>
2018-01-06 00:24:12 +0000

    Update validation form by binding event handler

--- view/frontend/web/js/rv-form-validator.js
+++ view/frontend/web/js/rv-form-validator.js
@@ -1,18 +1,15 @@
 define(['jquery'], function ($) {
-    return function (config, node) {
-        var $form = $(node);
-        $form.on('submit', function (e) {
-            var formValid = true;
-            $form.find(':input').each(function (i, input) {
-                if ($(input).hasClass('rv-validator-invalid')) {
-                    formValid = false;
-                }
-            });
-
-            if (!formValid) {
-                e.preventDefault();
-                return false;
+    return function (event) {
+        var formValid = true;
+        this.find(':input').each(function (i, input) {
+            if ($(input).hasClass('rv-validator-invalid')) {
+                formValid = false;
             }
         });
+
+        if (!formValid) {
+            event.preventDefault();
+            return false;
+        }
     }
 });
--- view/frontend/web/js/rv-input-validator.js
+++ view/frontend/web/js/rv-input-validator.js
@@ -1,35 +1,48 @@
-define(
-    ['jquery', 'mage/url', 'ucfirst', 'rv-input-results'],
-    function ($, url, ucfirst, inputResult) {
+define([
+        'jquery',
+        'mage/url',
+        'ucfirst',
+        'rv-input-results',
+        'rv-form-validator'
+    ],
+    function ($,
+              url,
+              ucfirst,
+              inputResult,
+              validator) {
 
-    return function (config, node) {
-        $(node).on('blur', function () {
-            var $this = $(this);
-            var data = {"input": $this.val()};
-            var rules = config['rules'];
+        return function (config, node) {
+            $(node).on('blur', function () {
+                var $this = $(this);
+                var data = {"input": $this.val()};
+                var rules = config['rules'];
 
-            if ($.isArray(rules)) {
-                $.each(rules, function (i, rule) {
-                    rule['class'] = ucfirst(rule['rule']);
-                });
-                data['rules'] = rules;
-            } else {
-                rules['class'] = ucfirst(rules['rule']);
-                data['rules'] = [rules];
-            }
-
-            $.ajax({
-                url: url.build('rest/v1/rv/validate/'+ JSON.stringify(data)),
-                type: "GET",
-                success: function (response) {
-                    var name = 'Input';
-                    if (config.hasOwnProperty('name')) {
-                        name = config['name'];
-                    }
-
-                    inputResult({"isValid": response, "name": name}, $this);
+                if ($.isArray(rules)) {
+                    $.each(rules, function (i, rule) {
+                        rule['class'] = ucfirst(rule['rule']);
+                    });
+                    data['rules'] = rules;
+                } else {
+                    rules['class'] = ucfirst(rules['rule']);
+                    data['rules'] = [rules];
                 }
+
+                $.ajax({
+                    url: url.build('rest/v1/rv/validate/' + JSON.stringify(data)),
+                    type: "GET",
+                    success: function (response) {
+                        var name = 'Input';
+                        if (config.hasOwnProperty('name')) {
+                            name = config['name'];
+                        }
+
+                        inputResult({"isValid": response, "name": name}, $this);
+                    }
+                });
+
+                var $form = $(this).closest('form');
+                $form.unbind('submit')
+                    .bind('submit', validator.bind($form));
             });
-        });
-    }
-});
+        }
+    });
